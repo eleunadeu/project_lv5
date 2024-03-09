dfdff66577844547b30954d86e437ccb643c25b3
sanha <0412csh@gmail.com>
2024-03-09 17:27:46 +0900

    fix: 장바구니 수정/삭제 (테스트완료)

--- src/main/java/com/sparta/goods/domain/cart/service/CartService.java
+++ src/main/java/com/sparta/goods/domain/cart/service/CartService.java
@@ -30,22 +30,21 @@ public class CartService {
     // 수정
     @Transactional
     public void updateCartItemQuantity(CartRequest requestDto) {
-        // 상품 ID를 사용하여 상품 엔티티를 찾습니다.
+        // 상품 ID를 사용하여 상품 엔티티 찾기
         Product product = productRepository.findById(requestDto.getProductId())
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 상품을 찾을 수 없습니다: " + requestDto.getProductId()));
 
-        // 상품이 속한 장바구니를 찾습니다.
+        // 상품이 속한 장바구니 찾기
         Cart cart = cartRepository.findByItemsProduct(product)
                 .orElseThrow(() -> new EntityNotFoundException("해당 상품을 포함한 장바구니를 찾을 수 없습니다."));
 
-        // 장바구니에서 상품을 나타내는 카트 항목을 찾습니다.
+        // 카트 찾기
         CartItem cartItem = findCartItemByProduct(cart, product);
 
-        // 만약 카트 항목이 존재하면, 수량을 업데이트하고 변경 사항을 데이터베이스에 저장합니다.
+        // 카트 항목이 존재하면 수량을 업데이트하고 변경 사항을 db에 저장
         if (cartItem != null) {
-            // 카트 항목의 수량을 요청된 수량으로 업데이트합니다.
+            // 카트 항목의 수량을 요청된 수량으로 업데이트
             cartItem.updateQuantity(requestDto.getCartQuantity());
-            // 변경된 카트를 저장합니다.
             cartRepository.save(cart);
         } else {
             throw new EntityNotFoundException("장바구니에서 상품을 찾을 수 없습니다: " + requestDto.getProductId());
